d8d5a7c50db7c369b0cd2002473e90db0196bae2
Max <bulanovich5@seznam.cz>
2024-02-18 22:45:11 +0100

    map integration

---
+++ lib/common_ui/restaurant_info_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:papi_burgers/constants/color_palette.dart';
+import 'package:papi_burgers/constants/sized_box.dart';
+
+class RestaurantInfoPage extends StatelessWidget {
+  final String logoUrl;
+  final String name;
+  final String address;
+  final String schedule;
+  const RestaurantInfoPage({
+    super.key,
+    required this.address,
+    required this.logoUrl,
+    required this.name,
+    required this.schedule,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.white,
+      content: SizedBox(
+        height: 177,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.network(logoUrl)),
+              title: Text(
+                name,
+                style: const TextStyle(
+                    fontSize: 18, color: grey4, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                address,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: grey9,
+                ),
+              ),
+            ),
+            h20,
+            Row(
+              children: [
+                Icon(Icons.watch_later_outlined),
+                const SizedBox(width: 5),
+                Text(
+                  'Время работы',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: grey7,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                schedule,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Color.fromARGB(255, 51, 51, 51),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/views/restaurants_map.dart
+++ lib/views/restaurants_map.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:papi_burgers/common_ui/restaurant_info_dialog.dart';
+import 'package:papi_burgers/constants/color_palette.dart';
 
 @RoutePage()
 class RestaurantMapPage extends StatefulWidget {
@@ -11,13 +13,90 @@ class RestaurantMapPage extends StatefulWidget {
 }
 
 class _RestaurantMapPageState extends State<RestaurantMapPage> {
+  GoogleMapController? mapController;
+
+  final LatLng initialLocation = LatLng(47.221809, 39.720261);
+  final LatLng markerLocation = LatLng(47.232257, 39.747759);
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  Set<Marker> _createMarker() {
+    return <Marker>[
+      Marker(
+        markerId: MarkerId('myLocation'),
+        position: markerLocation,
+        onTap: () {
+          _showInfoDialog();
+        },
+      ),
+    ].toSet();
+  }
+
+  void _showInfoDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return RestaurantInfoPage(
+            address: 'г.Ростов-на-Дону, Кировский район, проспект',
+            logoUrl:
+                'https://firebasestorage.googleapis.com/v0/b/papi-burgers-project.appspot.com/o/restaurants_images%2F%D0%BB%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF%20%D0%9F%D0%B0%D0%BF%D0%B8%20%D0%B1%D1%83%D1%80%D0%B3%D0%B5%D1%80_%D0%9C%D0%BE%D0%BD%D1%82%D0%B0%D0%B6%D0%BD%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%201%20%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F.png?alt=media&token=ff6fac81-57f4-43bc-9564-fcb227dafcca',
+            name: 'Papi burger’s',
+            schedule: 'круглосуточно');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          zoom: 14,
-          target: LatLng(50.090356, 14.302991),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: background,
+        body: Column(
+          children: [
+            const Spacer(),
+            Text(
+              'Наши проекты',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700),
+            ),
+            const Spacer(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 110,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: GoogleMap(
+                    compassEnabled: false,
+                    mapToolbarEnabled: false,
+                    zoomControlsEnabled: false,
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: initialLocation,
+                      zoom: 12.0,
+                    ),
+                    markers: _createMarker(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
